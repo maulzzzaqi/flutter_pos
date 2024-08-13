6f697f161008cfc97dbff93164f53a33a9f4db55
MaulzzZaqi <madezaqi2006@gmail.com>
2024-08-13 17:14:13 +0700

    app drawer

---
+++ lib/drawer/app_drawer.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_pos/authentication/auth_bloc/auth_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppDrawer extends StatelessWidget {
+  const AppDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: const Color(0xFF1A72DD),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.receipt_long_rounded,
+                  size: 50,
+                  color: Colors.white,
+                ),
+                Text(
+                  'POSapp',
+                  style: GoogleFonts.rubik(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.account_circle,
+              color: Colors.white,
+            ),
+            title: Text(
+              'Account',
+              style: GoogleFonts.rubik(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            onTap: () {
+              Navigator.of(context).pushNamed('/account');
+            },
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.restaurant_menu,
+              color: Colors.white,
+            ),
+            title: Text(
+              'Menu',
+              style: GoogleFonts.rubik(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            onTap: () {
+              // Navigator.of(context).pushNamed('/account');
+            },
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.logout,
+              color: Colors.white,
+            ),
+            title: Text(
+              'Logout',
+              style: GoogleFonts.rubik(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            onTap: () {
+              context.read<AuthBloc>().add(const AuthLogout());
+              Navigator.of(context).pushReplacementNamed('/welcome');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -3,8 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pos/authentication/auth_bloc/auth_bloc.dart';
 import 'package:flutter_pos/authentication/login_page.dart';
 import 'package:flutter_pos/cart/cart_page.dart';
+import 'package:flutter_pos/drawer/app_drawer.dart';
 import 'package:flutter_pos/menu/add_menu_page.dart';
 import 'package:flutter_pos/menu/menu_bloc/menu_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatefulWidget {
   static const String route = '/home';
@@ -24,66 +26,24 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Home Page'),
-        backgroundColor: Colors.blue.shade400,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => AddMenuPage()));
-            },
+        title: Text(
+          'POSapp',
+          style: GoogleFonts.rubik(
+            color: const Color(0xFF1A72DD),
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
           ),
-          IconButton(
-            icon: const Icon(Icons.shopping_cart),
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) => const CartPage()));
-            },
-          ),
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () {
-              context.read<AuthBloc>().add(const AuthLogout());
-              Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => LoginPage()));
-            },
-          )
-        ],
+        ),
+        centerTitle: true,
       ),
-      body: Column(
-        children: [
-          BlocBuilder<AuthBloc, AuthState>(
-            builder: (context, state) {
-              if (state.isLoading) {
-                return const Center(child: CircularProgressIndicator());
-              } else if (state.userData != null) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      const Text(
-                        'Currently logged in user:',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 24),
-                      ),
-                      Text('Email: ${state.userData?.email}'),
-                      Text('UID: ${state.userData?.uid}'),
-                      Text('Name: ${state.name}'),
-                      Text('Phone Number: ${state.phoneNumber}'),
-                      const SizedBox(height: 20),
-                    ],
-                  ),
-                );
-              } else {
-                return const Center(child: Text('No user data available.'));
-              }
-            },
-          ),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.of(context).pushNamed('/menu');
-            },
-            child: const Text('Go to menu page'),
-          )
-        ],
+      drawer: AppDrawer(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Home Page!'),
+          ],
+        ),
       ),
     );
   }
