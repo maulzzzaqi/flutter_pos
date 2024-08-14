86580c4c181299f970b0a6352cb5ee62e0eea0ad
MaulzzZaqi <madezaqi2006@gmail.com>
2024-08-15 01:21:21 +0700

    cart page ui

--- lib/cart/cart_page.dart
+++ lib/cart/cart_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pos/cart/cart_bloc/cart_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CartPage extends StatelessWidget {
   static const String route = '/cart';
@@ -10,59 +11,124 @@ class CartPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Your Cart'),
+        title: Text(
+          'Your Cart',
+          style: GoogleFonts.rubik(
+            color: const Color(0xFF1A72DD),
+            fontSize: 22,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: BlocBuilder<CartBloc, CartState>(
-        builder: (context, state) {
-          if (state is CartLoaded) {
-            return ListView.builder(
-              itemCount: state.items.length,
-              itemBuilder: (context, index) {
-                final item = state.items[index];
-                return ListTile(
-                  title: Text(item.name),
-                  subtitle: Text('Quantity: ${item.quantity}'),
-                  trailing: Column(
-                    children: [
-                      Text('Rp. ${item.price * item.quantity}'),
-                      Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.remove),
-                            onPressed: () {
-                              context
-                                  .read<CartBloc>()
-                                  .add(DecreaseQuantityEvent(item));
-                            },
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 8),
+        child: BlocBuilder<CartBloc, CartState>(
+          builder: (context, state) {
+            if (state is CartLoaded) {
+              final hasItems = state.items.isNotEmpty;
+
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: state.items.length,
+                      itemBuilder: (context, index) {
+                        final item = state.items[index];
+                        return ListTile(
+                          title: Text(
+                            item.name,
+                            style: GoogleFonts.rubik(
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500,
+                            ),
                           ),
-                          Text('${item.quantity}'),
-                          IconButton(
-                            icon: const Icon(Icons.add),
-                            onPressed: () {
-                              context
-                                  .read<CartBloc>()
-                                  .add(IncreaseQuantityEvent(item));
-                            },
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const SizedBox(height: 10),
+                              Text(
+                                'Quantity: ${item.quantity}',
+                                style: GoogleFonts.rubik(
+                                  color: Colors.black87,
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                              Text(
+                                'Rp. ${item.price * item.quantity}',
+                                style: GoogleFonts.rubik(
+                                  color: Color(0xFF1A72DD),
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                      IconButton(
-                        onPressed: () {
-                          context.read<CartBloc>().add(RemoveCartEvent(item));
-                        },
-                        icon: const Icon(Icons.delete),
-                      ),
-                    ],
+                          trailing: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: const Icon(Icons.remove),
+                                onPressed: () {
+                                  context
+                                      .read<CartBloc>()
+                                      .add(DecreaseQuantityEvent(item));
+                                },
+                              ),
+                              Text('${item.quantity}'),
+                              IconButton(
+                                icon: const Icon(Icons.add),
+                                onPressed: () {
+                                  context
+                                      .read<CartBloc>()
+                                      .add(IncreaseQuantityEvent(item));
+                                },
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  context.read<CartBloc>().add(RemoveCartEvent(item));
+                                },
+                                icon: const Icon(Icons.delete),
+                              ),
+                            ],
+                          ),
+                          isThreeLine: true,
+                        );
+                      },
+                    ),
                   ),
-                );
-              },
-            );
-          } else if (state is CartError) {
-            return Center(child: Text('Error: ${state.error}'));
-          }
-          return const Center(child: CircularProgressIndicator());
-        },
+                  if (hasItems)
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color(0xFF1A72DD),
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'Choose Payment Method',
+                          style: GoogleFonts.rubik(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              );
+            } else if (state is CartError) {
+              return Center(child: Text('Error: ${state.error}'));
+            }
+            return const Center(child: CircularProgressIndicator());
+          },
+        ),
       ),
     );
   }
