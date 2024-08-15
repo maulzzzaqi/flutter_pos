e9e93b4539e07854df012d40c8176fca7505eef4
MaulzzZaqi <madezaqi2006@gmail.com>
2024-08-15 16:22:08 +0700

    added cart page customer name field and ux improvements

--- lib/cart/cart_page.dart
+++ lib/cart/cart_page.dart
@@ -3,9 +3,27 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pos/cart/cart_bloc/cart_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CartPage extends StatelessWidget {
+class CartPage extends StatefulWidget {
   static const String route = '/cart';
   const CartPage({super.key});
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  final TextEditingController customerController = TextEditingController();
+  bool isButtonEnabled = false;
+
+  @override
+  void initState() {
+    super.initState();
+    customerController.addListener(() {
+      setState(() {
+        isButtonEnabled = customerController.text.isNotEmpty;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,9 +45,25 @@ class CartPage extends StatelessWidget {
           builder: (context, state) {
             if (state is CartLoaded) {
               final hasItems = state.items.isNotEmpty;
+              final hasNoItems = state.items.isEmpty;
+
+              final subtotal = state.items.fold<double>(
+                0.0,
+                (sum, item) => sum + item.price * item.quantity,
+              );
 
               return Column(
                 children: [
+                  if (hasNoItems)
+                    Text(
+                      'No item has been added.\nPlease add a menu to order!',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.rubik(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                   Expanded(
                     child: ListView.builder(
                       itemCount: state.items.length,
@@ -59,7 +93,7 @@ class CartPage extends StatelessWidget {
                               Text(
                                 'Rp. ${item.price * item.quantity}',
                                 style: GoogleFonts.rubik(
-                                  color: Color(0xFF1A72DD),
+                                  color: const Color(0xFF1A72DD),
                                   fontSize: 16,
                                   fontWeight: FontWeight.w500,
                                 ),
@@ -81,9 +115,7 @@ class CartPage extends StatelessWidget {
                               IconButton(
                                 icon: const Icon(Icons.add),
                                 onPressed: () {
-                                  context
-                                      .read<CartBloc>()
-                                      .add(IncreaseQuantityEvent(item));
+                                  context.read<CartBloc>().add(IncreaseQuantityEvent(item));
                                 },
                               ),
                               IconButton(
@@ -100,26 +132,76 @@ class CartPage extends StatelessWidget {
                     ),
                   ),
                   if (hasItems)
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          backgroundColor: const Color(0xFF1A72DD),
-                          padding: const EdgeInsets.symmetric(vertical: 16),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(Radius.circular(5)),
+                    Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              'Total Price: ',
+                              style: GoogleFonts.rubik(
+                                color: Colors.black87,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                            Text(
+                              'Rp. $subtotal',
+                              style: GoogleFonts.rubik(
+                                color: const Color(0xFF1A72DD),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        TextField(
+                          controller: customerController,
+                          decoration: InputDecoration(
+                            hintText: 'Customer Name',
+                            hintStyle: GoogleFonts.rubik(
+                              color: Colors.grey,
+                            ),
+                            filled: true,
+                            fillColor: Colors.grey.shade200,
+                            border: const OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20)),
+                                borderSide: BorderSide.none),
                           ),
                         ),
-                        onPressed: () {},
-                        child: Text(
-                          'Choose Payment Method',
-                          style: GoogleFonts.rubik(
-                            color: Colors.white,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
+                        const SizedBox(height: 10),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF1A72DD),
+                              padding: const EdgeInsets.symmetric(vertical: 16),
+                              shape: const RoundedRectangleBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(5)),
+                              ),
+                            ),
+                            onPressed: isButtonEnabled ? () {
+                              Navigator.pushNamed(context, '/payment');
+                            }
+                            : () {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(content: Text('Please enter the customer name!'))
+                              );
+                            }, 
+                            child: Text(
+                              'Choose Payment Method',
+                              style: GoogleFonts.rubik(
+                                color: Colors.white,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
                           ),
                         ),
-                      ),
+                      ],
                     ),
                 ],
               );
